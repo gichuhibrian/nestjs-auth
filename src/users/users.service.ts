72f3ad30252845235f26e51d154159963235545d
Brian Gichuhi <brianchegegichuhi@gmail.com>
2023-07-26 09:41:36 +0300

    creating user module and service with some sample json

---
+++ src/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'John Doe',
+      username: 'johndoe',
+      password: 'password123',
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      username: 'janedoe',
+      password: 'password456',
+    },
+  ];
+}
