eaa5f801cf34d652f4238a17079b8c428e7e0d70
Brian Gichuhi <brianchegegichuhi@gmail.com>
2023-07-26 10:33:13 +0300

    adding auth service as well as implementing passport-local strategy

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UsersService) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+
+    if (user && user.password === password) {
+      const { username, password, ...rest } = user;
+      return rest;
+    }
+    return null;
+  }
+}
